3c67c145539d38073a496035f27281aa32c0314a
vikash1200 <114206704+vikash1200@users.noreply.github.com>
2024-06-16 00:31:32 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -13,12 +13,12 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 logging.getLogger("telethon").setLevel(logging.WARNING)
 
 # variables
-API_ID = "24310324"#config("API_ID", default=None, cast=int)
-API_HASH = "c464afc9e1b8b2793a43fe8ece443bf5"#config("API_HASH", default=None)
-BOT_TOKEN = "7307527131:AAHQ4lqjf2m--TddjQRSWP_UrJ7WuO5et0g"#config("BOT_TOKEN", default=None)
-SESSION = "AQGqa00AQl_eHp1I_II8gEOSPHFBBz6ipjRGNL3sBdbuaiZwmFnWhGw4GFL0Yq1ryMWuGGZD-WePYdLMg4-nOxvOeJwjOTCpWnzzhs-1dYSaawq7nZuWy0YS43tTrU7jV9liA4T4_5pg6xtvSYj6qOFWG0La0hsozrei6ERBnHU5Yh3rz-ONWw6XL4Fhgz7vK9w8u-wKROYTB21mZoUES7bkeV7poOVOhjP8eYi_ejk8VtywLtZJPrLVleVFg-f2C-zSVETJsAeQkk_tjHlNKnzhHe4d-WUB53WaageHN0yboXvDees-z1S6oaUdLjmiSkO06RlwHwJT4xGMzlLxh9hywEYhgAAAAAFNVAIIAA" #config("SESSION", default=None)
-FORCESUB = "nachmeribulbul"#config("FORCESUB", default=None)
-AUTH = "5592318472"#config("AUTH", default=None)
+API_ID = config("API_ID", default=None, cast=int)
+API_HASH = config("API_HASH", default=None)
+BOT_TOKEN = config("BOT_TOKEN", default=None)
+SESSION = config("SESSION", default=None)
+FORCESUB = config("FORCESUB", default=None)
+AUTH = config("AUTH", default=None)
 SUDO_USERS = []
 
 if len(AUTH) != 0:
